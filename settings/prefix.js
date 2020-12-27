0026dc92b26572ea7f72884c78e7042d92844cc4
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2020-12-26 22:55:44 -0500

    Delete prefix.js

--- settings/prefix.js
+++
@@ -1,9 +0,0 @@
-const { prefix } = require('../../config/config')
-const { Schema, model } = require('mongoose')
-
-const settings = new Schema({
-    ID          :  { type: String },
-    GuildPrefix :  { type: String, default: prefix },
-})
-
-module.exports = model("Settings", settings)
